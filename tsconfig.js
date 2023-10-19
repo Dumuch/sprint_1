f91f19b73792af8fa1d1be911ee7e7b20d713e35
kizin_d <dumuch@yandex.ru>
2023-10-19 13:04:19 +0800

    Initial commit

---
+++ tsconfig.js
@@ -0,0 +1,55 @@
+// src/
+// ├── components/
+// │   └── button/
+// │       ├── button.*css
+// │       ├── button.js
+// │       ├── button.tmpl.js
+// │       └── index.js
+// ├── layout/
+// │   └── main/
+// │       ├── main.*css
+// │       ├── main.js
+// │       ├── main.tmpl.js
+// │       └── index.js
+// ├── modules/
+// │   └── chats/
+// │       ├── utils
+// │       │   └── getFullname.js
+// │       │   └── getIsOnline.js
+// │       ├── components/
+// │       │   └── message/
+// │       │       ├── message.*css
+// │       │       ├── message.js
+// │       │       ├── message.tmpl.js
+// │       │       └── index.js
+// │       ├── chats.*css
+// │       ├── chats.js
+// │       ├── chats.tmpl.js
+// │       └── index.js
+// ├── pages/
+// │   ├── home/
+// │   │   ├── modules/
+// │   │   │   ├── login/
+// │   │   │   │   ├── login.*css
+// │   │   │   │   ├── login.js
+// │   │   │   │   ├── login.tmpl.js
+// │   │   │   │   └── index.js
+// │   │   │   └── logout/
+// │   │   │       ├── logout.*css
+// │   │   │       ├── logout.js
+// │   │   │       ├── logout.tmpl.js
+// │   │   │       └── index.js
+// │   │   ├── home.*css
+// │   │   ├── home.js
+// │   │   ├── home.tmpl.js
+// │   │   └── index.js
+// │   └── about/
+// │       ├── about.*css
+// │       ├── about.js
+// │       ├── about.tmpl.js
+// │       └── index.js
+// └── utils/
+//     ├── get.js
+//     ├── http.js
+//     ├── isEmpty.js
+//     └── templator.js
